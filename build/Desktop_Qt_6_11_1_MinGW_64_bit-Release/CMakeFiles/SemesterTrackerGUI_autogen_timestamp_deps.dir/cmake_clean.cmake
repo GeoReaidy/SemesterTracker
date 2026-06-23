0f878fea1fa3798fc8b6c77e8bf2d4e811c6c42a
Georges Reaidy <georges.reaidy@lau.edu>
2026-06-23 12:22:40 +0300

    Delete build/Desktop_Qt_6_11_1_MinGW_64_bit-Release directory

--- build/Desktop_Qt_6_11_1_MinGW_64_bit-Release/CMakeFiles/SemesterTrackerGUI_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SemesterTrackerGUI_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
